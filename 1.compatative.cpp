902e60185a98fd27b7d2f6d1d3399fca97d7393d
Arvind kumar <115856872+arvind-crate-7@users.noreply.github.com>
2023-07-31 23:24:48 +0530

    Create 1.compatative.cpp

---
+++ 1.compatative.cpp
@@ -0,0 +1,31 @@
+/*
+Given rectangle of length of length l and b.print area of ractangle.
+
+Constrants:
+l<=l<=10^9
+l<=b<=10^9
+
+Input format:
+2 space seprate integer l and b
+
+Output Formate:
+Singly number which is area of rectangle
+
+Sample input:
+3 4
+Sample output:
+12
+
+*/
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int l,b;
+    cin>>l>>b;
+    cout<<l*b;
+}
